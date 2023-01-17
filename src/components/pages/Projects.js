3609b4c2b448fcfd435f14dffc3ddfba618e5a7f
Gabriel.M <gamotasantos2000@gmail.com>
2023-01-17 10:01:01 -0400

    Criando Dashboard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,22 +1,31 @@
-import {useLocation} from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 import Message from '../layouts/Message.js'
+import Container from '../layouts/Container.js'
+import LinkButton from '../layouts/LinkButton.js'
+
+import styles from './Projects.module.css'
 
 function Projects() {
 
     const location = useLocation()
 
     let message = ''
-    if(location.state){
+    if (location.state) {
         message = location.state.message
     }
 
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto"/>
+            </div>
             {message && <Message type="sucess" msg={message}/>}
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
         </div>
-
     )
 }
 
